2caca838a510fe784272d30094e88d18de990fd5
Priscila Nascimento <pnascimento2808@gmail.com>
2025-06-11 22:45:09 -0300

    Entrega do Projeto

---
+++ Contador/src/main/java/com/seuprojeto/contador/App.java
@@ -0,0 +1,6 @@
+package com.seuprojeto.contador;
+
+public class App {
+    public static void main(String[] args) {
+    }
+} 
---
+++ Contador/src/main/java/com/seuprojeto/contador/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package com.seuprojeto.contador;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+} 
